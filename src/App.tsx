4c742331e2be376d86ee870f3ac771a5f9d7f00c
Dev <devendragaud@idcletech.com>
2023-02-13 16:53:32 +0530

    showing currencies in table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import axios from "axios"
+
 function App() {
+  const [currencies,setCurrencies] = useState<[string, string][]>([])
+  useEffect(() => {
+    axios.get("https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json")
+    .then((response: any) => {
+      setCurrencies(Object.entries(response.data));
+    })
+  },[]);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <table >
+       <thead>
+         <tr>
+           <th>Code</th>
+           <th>Currency</th>
+         </tr>
+       </thead>
+       <tbody >
+         {currencies.map((ele)=>(
+           <tr key={ele[0]}>
+             <td>{ele[0]}</td>
+             <td>{ele[1]}</td>
+           </tr>
+         ))}
+       </tbody>
+     </table>
     </div>
   );
 }
-
 export default App;
